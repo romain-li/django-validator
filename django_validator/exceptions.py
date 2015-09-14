0bcecfdf33f42f85bb9a8e32e79686a41fb5226a
lixue <romain_li@163.com>
2015-09-14 16:22:40 +0800

    Fix Validation import error in older DRF.

--- django_validator/exceptions.py
+++ django_validator/exceptions.py
@@ -2,10 +2,10 @@ from rest_framework import status
 import rest_framework.exceptions
 
 
-class ValidationError(rest_framework.exceptions.ValidationError):
+class ValidationError(rest_framework.exceptions.APIException):
     code = ''
 
     def __init__(self, detail, code=None, status_code=status.HTTP_400_BAD_REQUEST):
         super(ValidationError, self).__init__(detail)
-        self.status_code = status_code
         self.code = code
+        self.status_code = status_code
