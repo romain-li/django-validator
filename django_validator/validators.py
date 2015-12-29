858c919bb73939b918b61fe910115c3962e778fe
duoduo369 <duoduo3369@gmail.com>
2015-12-29 11:18:49 +0800

    fix require中某些版本django过来的字符串是unicode的问题

--- django_validator/validators.py
+++ django_validator/validators.py
@@ -127,7 +127,7 @@ class RequiredValidator(BaseValidator):
     def required_valid(value):
         if value is None:
             return False
-        elif isinstance(value, str) and value.strip() == '':
+        elif isinstance(value, basestring) and value.strip() == '':
             return False
         return True
 
